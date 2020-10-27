3d35f4abfc017a7ec53cfbf600088187bb8630e6
jack <mzyaimll@users.noreply.github.com>
2020-10-27 22:28:43 +0800

    update main.vue

--- machine-geek/src/views/main.vue
+++ machine-geek/src/views/main.vue
@@ -2,26 +2,73 @@
   <a-layout id="components-layout-demo-custom-trigger">
     <a-layout-sider v-model:collapsed="collapsed" :trigger="null" collapsible>
       <div class="logo" />
-      <a-menu theme="dark" mode="inline" @click="handleClick" v-model:selectedKeys="selectedKeys">
-        <a-menu-item v-for="(item) in routers" :key="item.path">
+      <a-menu
+        theme="dark"
+        mode="inline"
+        @click="handleClick"
+        v-model:selectedKeys="selectedKeys"
+      >
+        <a-menu-item v-for="item in routers" :key="item.path">
           <router-link :to="item.path">
             <user-outlined />
-            <span>{{item.meta.title}}</span>
+            <span>{{ item.meta.title }}</span>
           </router-link>
         </a-menu-item>
       </a-menu>
     </a-layout-sider>
     <a-layout>
-      <a-layout-header style="background: #fff; padding: 0">
+      <a-layout-header class="main-header" style="background: #fff; padding: 0">
         <menu-unfold-outlined
           v-if="collapsed"
           class="trigger"
           @click="() => (collapsed = !collapsed)"
         />
-        <menu-fold-outlined v-else class="trigger" @click="() => (collapsed = !collapsed)" />
+        <menu-fold-outlined
+          v-else
+          class="trigger"
+          @click="() => (collapsed = !collapsed)"
+        />
+        <a-menu v-model:selectedKeys="current" mode="horizontal">
+          <a-menu-item key="mail">
+            <mail-outlined />Navigation One
+          </a-menu-item>
+          <a-menu-item key="app" disabled>
+            <appstore-outlined />Navigation Two
+          </a-menu-item>
+          <a-sub-menu>
+            <template v-slot:title>
+              <span class="submenu-title-wrapper">
+                <setting-outlined />
+                Navigation Three - Submenu
+              </span>
+            </template>
+            <a-menu-item-group title="Item 1">
+              <a-menu-item key="setting:1"> Option 1 </a-menu-item>
+              <a-menu-item key="setting:2"> Option 2 </a-menu-item>
+            </a-menu-item-group>
+            <a-menu-item-group title="Item 2">
+              <a-menu-item key="setting:3"> Option 3 </a-menu-item>
+              <a-menu-item key="setting:4"> Option 4 </a-menu-item>
+            </a-menu-item-group>
+          </a-sub-menu>
+          <a-menu-item key="alipay">
+            <a
+              href="https://antdv.com"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Navigation Four - Link
+            </a>
+          </a-menu-item>
+        </a-menu>
       </a-layout-header>
       <a-layout-content
-        :style="{ margin: '24px 16px', padding: '24px', background: '#fff', minHeight: '280px' }"
+        :style="{
+          margin: '24px 16px',
+          padding: '24px',
+          background: '#fff',
+          minHeight: '280px',
+        }"
       >
         <router-view></router-view>
       </a-layout-content>
@@ -35,10 +82,10 @@ import {
   UploadOutlined,
   MenuUnfoldOutlined,
   MenuFoldOutlined,
-} from '@ant-design/icons-vue';
-import routes from '@/router/index'
+} from "@ant-design/icons-vue";
+import routes from "@/router/index";
 
-import {defineComponent} from 'vue'
+import { defineComponent } from "vue";
 
 export default defineComponent({
   components: {
@@ -48,24 +95,24 @@ export default defineComponent({
     MenuUnfoldOutlined,
     MenuFoldOutlined,
   },
-  setup(props,context) {
-    return {}
+  setup(props, context) {
+    return {};
   },
   data() {
     return {
       routers: [],
-      selectedKeys: ['1'],
+      selectedKeys: ["1"],
       collapsed: false,
     };
   },
   methods: {
     handleClick(e) {
       // this.$router.push(e.key)
-    }
+    },
   },
   mounted() {
-    this.routers = routes.options.routes[0].children
-  }
+    this.routers = routes.options.routes[0].children;
+  },
 });
 </script>
 <style>
@@ -89,5 +136,11 @@ export default defineComponent({
   height: 32px;
   background: rgba(255, 255, 255, 0.2);
   margin: 16px;
+}
+
+.main-header {
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
 }
 </style>
