d60b6c364f3552bf12e54d8a11e6f07a71c8916c
jack <mzyaimll@users.noreply.github.com>
2020-10-27 23:47:34 +0800

    update main

--- machine-geek/src/views/main.vue
+++ machine-geek/src/views/main.vue
@@ -28,39 +28,6 @@
           class="trigger"
           @click="() => (collapsed = !collapsed)"
         />
-        <a-menu v-model:selectedKeys="current" mode="horizontal">
-          <a-menu-item key="mail">
-            <mail-outlined />Navigation One
-          </a-menu-item>
-          <a-menu-item key="app" disabled>
-            <appstore-outlined />Navigation Two
-          </a-menu-item>
-          <a-sub-menu>
-            <template v-slot:title>
-              <span class="submenu-title-wrapper">
-                <setting-outlined />
-                Navigation Three - Submenu
-              </span>
-            </template>
-            <a-menu-item-group title="Item 1">
-              <a-menu-item key="setting:1"> Option 1 </a-menu-item>
-              <a-menu-item key="setting:2"> Option 2 </a-menu-item>
-            </a-menu-item-group>
-            <a-menu-item-group title="Item 2">
-              <a-menu-item key="setting:3"> Option 3 </a-menu-item>
-              <a-menu-item key="setting:4"> Option 4 </a-menu-item>
-            </a-menu-item-group>
-          </a-sub-menu>
-          <a-menu-item key="alipay">
-            <a
-              href="https://antdv.com"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Navigation Four - Link
-            </a>
-          </a-menu-item>
-        </a-menu>
       </a-layout-header>
       <a-layout-content
         :style="{
