993cd39ed5c5aaf5ba7f02e9364638fd6aca82f5
mzyaimll <2592972482@qq.com>
2020-11-20 00:52:10 +0800

    update api

--- src/api/modules/systemException.ts
+++ src/api/modules/systemException.ts
@@ -21,13 +21,19 @@
  * @Author: JackM
  * @Date: 2020-11-02 22:01:53
  * @LastEditors: JackM
- * @LastEditTime: 2020-11-12 00:29:58
+ * @LastEditTime: 2020-11-20 00:52:00
  */
 
 import request from '/@/utils/request'
 
 export default {
     systemException_paging(params: any) {
+        return request<{ data: [], success: boolean }>({
+            url: '/systemException/paging?page=' + params.page + '&size=' + params.size,
+            method: "get",
+        })
+    },
+    systemException_find(params: any) {
         return request<{ data: [], success: boolean }>({
             url: '/systemException/paging?page=' + params.page + '&size=' + params.size,
             method: "get",
