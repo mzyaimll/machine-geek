ad758d022313bcd78e85ca3be9ebbd48963ac718
mzyaimll <2592972482@qq.com>
2020-11-27 19:39:48 +0800

    update project

--- src/views/Setting.vue
+++ src/views/Setting.vue
@@ -50,12 +50,12 @@
  * @Author: JackM
  * @Date: 2020-11-02 22:01:53
  * @LastEditors: JackM
- * @LastEditTime: 2020-11-24 20:23:41
+ * @LastEditTime: 2020-11-27 19:39:36
  -->
 
 <template>
   <div>
-    <h1>This is a setting page</h1>
+    <h1>This is a setting page yes this is a setting page</h1>
     <p>store count is: {{ count }}</p>
   </div>
 </template>
